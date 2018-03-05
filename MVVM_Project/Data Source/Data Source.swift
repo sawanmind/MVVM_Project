ed036188bca3372f02db33f272e5933cc0e5d9c0
iOS Development <iosdevelopment@iOSs-Mac-mini.local>
2018-03-05 14:05:57 +0530

    added generic data source for collectionView

--- MVVM_Project/Data Source/Data Source.swift
+++ MVVM_Project/Data Source/Data Source.swift
@@ -17,7 +17,7 @@ class TableViewDataSource<Cell :UITableViewCell,ViewModel> : NSObject, UITableVi
     
     
     init(cellIdentifier :String,tableView:UITableView ,items :[ViewModel], configureCell: @escaping (Cell,ViewModel) -> ()) {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.register(Cell.self, forCellReuseIdentifier: cellIdentifier)
         self.cellIdentifier = cellIdentifier
         self.items = items
         self.configureCell = configureCell
@@ -36,5 +36,57 @@ class TableViewDataSource<Cell :UITableViewCell,ViewModel> : NSObject, UITableVi
     }
     
 }
+
+
+
+
+class CollectionViewDataSource<Cell:UICollectionViewCell, ViewModel> : NSObject , UICollectionViewDataSource {
+    private var cellIdentifier :String!
+    private var items :[ViewModel]!
+    var configureCell :(Cell,ViewModel) -> ()
+    
+
+    init(cellIdentifier :String,collectionView:UICollectionView ,items :[ViewModel], configureCell: @escaping (Cell,ViewModel) -> ()) {
+        collectionView.register(Cell.self, forCellWithReuseIdentifier: cellIdentifier)
+        self.cellIdentifier = cellIdentifier
+        self.items = items
+        self.configureCell = configureCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath) as! Cell
+        let item = self.items[indexPath.row]
+        self.configureCell(cell,item)
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
